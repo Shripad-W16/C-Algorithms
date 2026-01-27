d69da6835a9f0109811b3bfeb5a2e0442a08102a
Shripad Ingle <shripadingle11@gmail.com>
2026-01-27 23:02:49 +0530

    even_odd.c
    This program checks whether a given integer is even or odd using the modulus operator.
    It demonstrates understanding of conditional statements (if-else), arithmetic operators, and user input handling.

---
+++ even_odd.c
@@ -0,0 +1,17 @@
+//codes by shripad ->>
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is an Even number.\n", num);
+    } else {
+        printf("%d is an Odd number.\n", num);
+    }
+
+    return 0;
+}
